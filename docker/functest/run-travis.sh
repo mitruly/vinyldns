48775f2e388659cc587c3a91b0fe1aa9d6c350a5
Michael Ly <michaeltrulyng@gmail.com>
2018-08-02 00:36:24 -0400

    Testing

--- docker/functest/run-travis.sh
+++ docker/functest/run-travis.sh
@@ -8,7 +8,7 @@ VINYLDNS_URL="http://localhost:9000"
 echo "Waiting for API to be ready at ${VINYLDNS_URL} ..."
 DATA=""
 RETRY=40
-while [ $RETRY -gt 0 ]
+while [ "$RETRY" -gt 0 ]
 do
     DATA=$(wget -O - -q -t 1 "${VINYLDNS_URL}/ping")
     if [ $? -eq 0 ]
@@ -20,7 +20,7 @@ do
         let RETRY-=1
         sleep 1
 
-        if [ $RETRY -eq 0 ]
+        if [ "$RETRY" -eq 0 ]
         then
           echo "Exceeded retries waiting for VINYLDNS to be ready, failing"
           exit 1
@@ -31,6 +31,6 @@ done
 DNS_IP="http://localhost:53"
 echo "Running live tests against ${VINYLDNS_URL} and DNS server ${DNS_IP}"
 
-chmod +x $DIR/run-tests.py
-ls -l $DIR
-$DIR/run-tests.py functional_test/live_tests -v #--url="$VINYLDNS_URL" --dns-ip="$DNS_IP"
+chmod +x "$DIR"/run-tests.py
+ls -l "$DIR"
+"$DIR"/run-tests.py functional_test/live_tests/* -v #--url="$VINYLDNS_URL" --dns-ip="$DNS_IP"
