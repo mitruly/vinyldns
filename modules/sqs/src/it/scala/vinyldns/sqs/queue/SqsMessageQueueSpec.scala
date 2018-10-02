a2790103343263658509363e24ea9e3a54657a8f
Michael Ly <michaeltrulyng@gmail.com>
2018-10-02 16:57:25 -0400

    WIP

---
+++ modules/sqs/src/it/scala/vinyldns/sqs/queue/SqsMessageQueueSpec.scala
@@ -0,0 +1,44 @@
+
+package vinyldns.sqs.queue
+
+import org.scalatest.{BeforeAndAfterAll, WordSpec}
+import org.scalatest.mockito.MockitoSugar
+import vinyldns.core.queue.CommandMessage
+import vinyldns.core.TestRecordSetData._
+
+import org.scalatest.Matchers
+
+class SqsMessageQueueSpec extends WordSpec with MockitoSugar with BeforeAndAfterAll with Matchers {
+  private var queue: SqsMessageQueue = _
+
+  "SqsMessageQueueSpec" should {
+    "receive a message from the queue" in {
+    }
+
+    "remove a message from the queue" in {
+
+    }
+
+    "return unit when attempting to requeue" in {
+      queue = SqsMessageQueue()
+      queue.requeue(CommandMessage(ReceiptHandle("test"), makeTestAddChange(rsOk))).unsafeRunSync() shouldBe 1
+    }
+
+    "send single message request" in {
+      queue = SqsMessageQueue()
+      queue.send(makeTestAddChange(rsOk)).unsafeRunSync() shouldBe 1
+    }
+
+    "send batch message requests" in {
+
+    }
+
+    "change message visibility timeouts" in {
+
+    }
+
+    "shutdown" in {
+
+    }
+  }
+}
--- modules/sqs/src/main/scala/vinyldns/sqs/queue/SqsMessageQueue.scala
+++ modules/sqs/src/main/scala/vinyldns/sqs/queue/SqsMessageQueue.scala
@@ -18,10 +18,17 @@ package vinyldns.sqs.queue
 
 import cats.data.NonEmptyList
 import cats.effect.IO
+import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
+import com.amazonaws.handlers.AsyncHandler
+import com.amazonaws.{AmazonWebServiceRequest, AmazonWebServiceResult}
+import com.amazonaws.services.sqs.{AmazonSQSAsync, AmazonSQSAsyncClientBuilder}
 import com.amazonaws.services.sqs.model._
+import com.typesafe.config.{Config, ConfigFactory}
 import vinyldns.core.domain.zone.ZoneCommand
 import vinyldns.core.protobuf.ProtobufConversions
 import vinyldns.core.queue._
+import vinyldns.core.route.Monitor
 
 import scala.concurrent.duration.FiniteDuration
 import scala.collection.JavaConverters._
@@ -29,10 +36,41 @@ import vinyldns.sqs.queue.SqsConverters._
 
 final case class ReceiptHandle(value: String) extends MessageHandle
 
-class SqsMessageQueue(liveSqsConnection: LiveSqsConnection)
+class SqsMessageQueue(queueUrl: String, client: AmazonSQSAsync)
     extends MessageQueue
     with ProtobufConversions {
-  import liveSqsConnection._
+
+  private def monitored[A](name: String)(f: => IO[A]): IO[A] = {
+    val monitor = Monitor(name)
+    val startTime = System.currentTimeMillis
+
+    def timeAndRecord: Boolean => Unit = monitor.capture(monitor.duration(startTime), _)
+
+    def failed(): Unit = timeAndRecord(false)
+    def succeeded(): Unit = timeAndRecord(true)
+
+    f.attempt.flatMap {
+      case Left(error) =>
+        failed()
+        IO.raiseError(error)
+      case Right(ok) =>
+        succeeded()
+        IO.pure(ok)
+    }
+  }
+
+  private def sqsAsync[A <: AmazonWebServiceRequest, B <: AmazonWebServiceResult[_]](
+      request: A,
+      f: (A, AsyncHandler[A, B]) => java.util.concurrent.Future[B]): IO[B] =
+    IO.async[B] { complete: (Either[Throwable, B] => Unit) =>
+      val asyncHandler = new AsyncHandler[A, B] {
+        def onError(exception: Exception): Unit = complete(Left(exception))
+
+        def onSuccess(request: A, result: B): Unit = complete(Right(result))
+      }
+
+      f(request, asyncHandler)
+    }
 
   def receive(count: MessageCount): IO[List[CommandMessage]] =
     monitored("sqs.receiveMessageBatch") {
@@ -65,6 +103,7 @@ class SqsMessageQueue(liveSqsConnection: LiveSqsConnection)
 
   def send[A <: ZoneCommand](messages: NonEmptyList[A]): IO[SendBatchResult] =
     IO.pure(SendBatchResult(List(), List()))
+
   /*
   def send[A <: ZoneCommand](messages: NonEmptyList[A]): IO[SendBatchResult] =
     monitored("sqs.sendMessageBatch")(
@@ -85,8 +124,30 @@ class SqsMessageQueue(liveSqsConnection: LiveSqsConnection)
       sqsAsync[ChangeMessageVisibilityRequest, ChangeMessageVisibilityResult](
         new ChangeMessageVisibilityRequest()
           .withReceiptHandle(message.handle.asInstanceOf[ReceiptHandle].value)
-          .withVisibilityTimeout(1600) // 1800 seconds == 30 minutes
+          .withVisibilityTimeout(1800) // 1800 seconds == 30 minutes
           .withQueueUrl(queueUrl),
         client.changeMessageVisibilityAsync
       )).map(_ => ())
+
+  def shutdown(): Unit = client.shutdown()
+}
+
+object SqsMessageQueue {
+  def apply(config: Config = ConfigFactory.load().getConfig("sqs")): SqsMessageQueue = {
+    val accessKey = config.getString("access-key")
+    val secretKey = config.getString("secret-key")
+    val serviceEndpoint = config.getString("service-endpoint")
+    val signingRegion = config.getString("signing-region")
+    val queueUrl = config.getString("queue-url")
+
+    val client =
+      AmazonSQSAsyncClientBuilder
+        .standard()
+        .withEndpointConfiguration(new EndpointConfiguration(serviceEndpoint, signingRegion))
+        .withCredentials(
+          new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+        .build()
+
+    new SqsMessageQueue(queueUrl, client)
+  }
 }
